59c3f131406137c24461f95daf49b7b7c1f11f29
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-02 06:59:51 +0000

    Deploy Mavlon90/yandex-m to github.com/Mavlon90/yandex-m.git:gh-pages

---
+++ precache-manifest.9526f0882eae1bc507476cc6a3e1914d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873572260a8a3f1b60efca6b82dc389d",
+    "url": "/yandex-m/index.html"
+  },
+  {
+    "revision": "cda665fc2b8282497076",
+    "url": "/yandex-m/static/css/main.095b80f3.chunk.css"
+  },
+  {
+    "revision": "cb0689d78f3cfa231b93",
+    "url": "/yandex-m/static/js/2.f9287e21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/yandex-m/static/js/2.f9287e21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cda665fc2b8282497076",
+    "url": "/yandex-m/static/js/main.9488cc0f.chunk.js"
+  },
+  {
+    "revision": "b72378cd509469be468e",
+    "url": "/yandex-m/static/js/runtime-main.7150fcc5.js"
+  }
+]);
